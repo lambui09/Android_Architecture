7fa2fb25336a5da82669320f07d67a59543488a5
Bui Duc Lam <buiduclam09@gmail.com>
2020-08-01 10:46:40 +0700

    update add api

--- app/src/main/java/com/lambui09/mvvm/data/Constants.kt
+++ app/src/main/java/com/lambui09/mvvm/data/Constants.kt
@@ -1,6 +1,6 @@
 package com.lambui09.mvvm.data
 
 object Constants {
-    const val BASE_URL = "https://abc.xyz"
+    const val BASE_URL = "http://35.239.148.89:3000/api/v1/"
     const val DELAY_BUTTON_CLICKED = 300L //300 ms
 }
--- app/src/main/java/com/lambui09/mvvm/ui/task_detail/TaskDetailFragment.kt
+++ app/src/main/java/com/lambui09/mvvm/ui/task_detail/TaskDetailFragment.kt
@@ -1,11 +1,19 @@
 package com.lambui09.mvvm.ui.task_detail
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.lambui09.mvvm.R
 import com.lambui09.mvvm.ui.base.fragment.BaseFragment
+import kotlinx.android.synthetic.main.fragment_task_detail.*
 
 class TaskDetailFragment : BaseFragment() {
 
@@ -14,6 +22,12 @@ class TaskDetailFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_task_detail, container, false)
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 }
